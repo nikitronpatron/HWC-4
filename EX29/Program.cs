e0acd6156c4d4f2cf7c3c0b98eec967544fdd430
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-14 09:54:53 +0300

    EX29

---
+++ EX29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(101);
+    }
+    return result;
+}
+
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(",", array)}] ");
