1bcef44cb5d41d5590229a6ed38b4b9f6930124b
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-14 09:53:58 +0300

    EX25

---
+++ EX25/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход два числа (A и B)
+// возводит число A в натуральную степень B
+
+int Exponentiation(int a, int b)
+{
+    int result = 1;
+    for(int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int exp = Exponentiation(x, y);
+Console.WriteLine(exp);
