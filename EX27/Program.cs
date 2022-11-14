19d8a8f387d253b8cf0627fecd716c68a31fb0a9
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-14 09:54:26 +0300

    EX27

---
+++ EX27/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход число
+// выдаёт сумму цифр в числе
+
+int AddDigitsNumber(int num)
+{
+    int result = 0;
+    while(num != 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = AddDigitsNumber(number);
+Console.WriteLine(sum);
